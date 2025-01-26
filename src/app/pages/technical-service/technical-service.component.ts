3d61c0f73951ed8c4a744bd2dae8b8bab1063bee
Egor <egsrapps@gmail.com>
2025-01-26 13:39:43 +0100

    Services

--- src/app/pages/technical-service/technical-service.component.ts
+++ src/app/pages/technical-service/technical-service.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { TopComponent } from '../../shaders/top/top.component';
 import { AboutComponent } from './about/about.component';
+import { ServicesComponent } from './services/services.component';
 
 @Component({
   selector: 'app-technical-service',
   standalone: true,
-  imports: [TopComponent, AboutComponent],
+  imports: [TopComponent, AboutComponent, ServicesComponent],
   templateUrl: './technical-service.component.html',
   styleUrl: './technical-service.component.scss'
 })
