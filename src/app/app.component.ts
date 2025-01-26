e4614b5ff3991e217b3626b84a8c1ec4e98d8c37
Egor <egsrapps@gmail.com>
2025-01-26 16:18:08 +0100

    Loader

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,11 +2,12 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from './shaders/header/header.component';
 import { FooterComponent } from "./shaders/footer/footer.component";
+import { LoaderComponent } from './shaders/loader/loader.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, HeaderComponent, FooterComponent],
+  imports: [RouterOutlet, HeaderComponent, FooterComponent, LoaderComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
