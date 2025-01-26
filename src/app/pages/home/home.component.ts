ad3123fd45fec381b56915dedd10deda6ee42038
Egor <egsrapps@gmail.com>
2025-01-26 02:47:51 +0100

    Form

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -5,11 +5,12 @@ import { GoSolutionsComponent } from "./components/go-solutions/go-solutions.com
 import { CaseStudiesComponent } from "../../shaders/case-studies/case-studies.component";
 import { AdvantagesComponent } from "./components/advantages/advantages.component";
 import { WorkComponent } from './components/work/work.component';
+import { ContactComponent } from '../../shaders/contact/contact.component';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [TopComponent, SolutionsComponent, GoSolutionsComponent, CaseStudiesComponent, AdvantagesComponent, WorkComponent],
+  imports: [TopComponent, SolutionsComponent, GoSolutionsComponent, CaseStudiesComponent, AdvantagesComponent, WorkComponent, ContactComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss'
 })
