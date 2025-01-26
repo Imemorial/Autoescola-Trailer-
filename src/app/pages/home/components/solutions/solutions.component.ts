fb628f396d2bc0bf1c057e858e4b0febf5606f27
Egor <egsrapps@gmail.com>
2025-01-26 01:20:06 +0100

    Optimitzation

--- src/app/pages/home/components/solutions/solutions.component.ts
+++ src/app/pages/home/components/solutions/solutions.component.ts
@@ -1,16 +1,74 @@
 import { Component, AfterViewInit } from '@angular/core';
 import Splide from '@splidejs/splide';
 import { Inject, PLATFORM_ID } from '@angular/core';
-import { isPlatformBrowser } from '@angular/common';
+import { isPlatformBrowser, NgFor } from '@angular/common';
 
 @Component({
   selector: 'app-solutions',
   standalone: true,
-  imports: [],
+  imports: [NgFor],
   templateUrl: './solutions.component.html',
   styleUrls: ['./solutions.component.scss']
 })
 export class SolutionsComponent implements AfterViewInit {
+
+  slides = [
+    {
+      title: 'Diseño Web',
+      description:
+        'Creando interfaces atractivas y funcionales que ofrecen una experiencia de usuario óptima.',
+      features: [
+        'Diseñar interfaces intuitivas y responsivas.',
+        'Implementar tipografía y colores atractivos.',
+        'Mejorar la navegación para una experiencia fluida.',
+        'Incorporar tendencias de diseño modernas.',
+      ],
+    },
+    {
+      title: 'Software Personalizado',
+      description:
+        'Tienes una idea para mejorar tu negocio o tu forma de trabajar. Nosotros la hacemos realidad con un programa hecho a tu medida.',
+      features: [
+        'Crea herramientas únicas para tus tareas diarias.',
+        'Organiza tu negocio y hazlo más eficiente.',
+        'Diseñamos soluciones basadas en tus ideas.',
+        'Ahorra tiempo y mejora tus resultados.',
+      ],
+    },
+    {
+      title: 'Tiendas Online (ECOMMERCE)',
+      description:
+        'Creemos tu tienda online para que vendas tus productos o servicios de forma fácil, rápida y profesional.',
+      features: [
+        'Diseñamos tu tienda online lista para vender.',
+        'Fácil de usar para ti y tus clientes.',
+        'Métodos de pago seguros y confiables.',
+        'Gestión de inventarios y pedidos automatizada.',
+      ],
+    },
+    {
+      title: 'Servicio Técnico y Mantenimiento',
+      description:
+        'Mantén tus computadoras y equipos funcionando al 100% con nuestro servicio técnico confiable.',
+      features: [
+        'Arreglamos problemas con computadoras y laptops.',
+        'Limpieza y optimización para que sean más rápidas.',
+        'Instalación de programas y actualización de software.',
+        'Mantenimiento preventivo para evitar fallas futuras.',
+      ],
+    },
+    {
+      title: 'Instalación de Cámaras de Seguridad',
+      description:
+        'Protege tu hogar o negocio con cámaras de seguridad instaladas por profesionales.',
+      features: [
+        'Instalación rápida y profesional en cualquier espacio.',
+        'Cámaras de alta definición para vigilancia clara.',
+        'Acceso remoto desde tu celular o computadora.',
+        'Soluciones personalizadas para tu seguridad.',
+      ],
+    },
+  ];
 
   constructor(@Inject(PLATFORM_ID) private platformId: Object) {}
 
--- src/app/shaders/case-studies/case-studies.component.ts
+++ src/app/shaders/case-studies/case-studies.component.ts
@@ -1,27 +1,65 @@
-import { isPlatformBrowser } from '@angular/common';
+import { isPlatformBrowser, NgFor } from '@angular/common';
 import { Component, Inject, PLATFORM_ID } from '@angular/core';
 import Splide from '@splidejs/splide';
 
 @Component({
   selector: 'app-case-studies',
   standalone: true,
-  imports: [],
+  imports: [NgFor],
   templateUrl: './case-studies.component.html',
   styleUrl: './case-studies.component.scss'
 })
 export class CaseStudiesComponent {
 
+    cases = [
+    {
+      title: 'El Mirall Escola de Ball',
+      category: 'Software',
+      description: 'Creamos un sitio web moderno y funcional para "El Mirall Escola de Ball", una academia de baile ubicada en Lleida. La plataforma está diseñada para reflejar la pasión por el baile y permite a los usuarios consultar horarios, inscribirse en clases y conocer más sobre la academia.',
+      image: '/assets/images/elmirall.png',
+      alt: 'El Mirall Escola de Ball'
+    },
+    {
+      title: 'Gloder Bellpuig',
+      category: 'Software',
+      description: 'Desarrollamos un sitio web atractivo y fácil de usar para Gloder Bellpuig, un restaurante destacado en Bellpuig. La página incluye funcionalidades como menús digitales, reservas en línea y una sección para descubrir la historia del restaurante.',
+      image: '/assets/images/gloder.png',
+      alt: 'Gloder Bellpuig'
+    },
+    {
+      title: 'Asere Academia de Ball Bellpuig',
+      category: 'Software',
+      description: 'Diseñamos y desarrollamos un sitio web intuitivo para "Asere Academia de Ball", una academia de baile ubicada en Bellpuig. El proyecto se centra en proporcionar información clara sobre las clases, eventos y actividades especiales, creando una experiencia fluida para estudiantes y visitantes.',
+      image: '/assets/images/asere.png',
+      alt: 'Asere Academia de Ball Bellpuig'
+    },
+    {
+      title: 'Gastrobacus',
+      category: 'Software',
+      description: 'Implementamos una solución completa de web app para Gastrobacus, una asociación de Lleida enfocada en la gestión eficiente de sus finanzas. La aplicación facilita el control de gastos, el análisis presupuestario y la colaboración entre los miembros de la asociación.',
+      image: '/assets/images/gastrobacus.png',
+      alt: 'Gastrobacus'
+    },
+    {
+      title: 'Eselmind',
+      category: 'Software',
+      description: 'Desarrollamos una plataforma innovadora para Eselmind, una academia de finanzas que busca empoderar a sus estudiantes con conocimientos prácticos. El sitio web incluye recursos educativos, programas personalizados y herramientas para el aprendizaje interactivo.',
+      image: '/assets/images/eselmind.png',
+      alt: 'Eselmind'
+    }
+  ];
+
   constructor(@Inject(PLATFORM_ID) private platformId: Object) {}
 
   ngAfterViewInit(): void {
     if (isPlatformBrowser(this.platformId)) {
-      const splide = new Splide('.splide2', {
+      const splide = new Splide('.case-studies-slider', {
         type: 'loop',
         perPage: 3,
         perMove: 1,
-        height: '400px',
         speed: 1000,
         arrows: false, 
+        gap: '1rem',
         pagination: false,
       }).mount();
   
