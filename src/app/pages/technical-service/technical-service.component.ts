90ce0e560dc14112be3e74d23fbf24db55033236
Egor <egsrapps@gmail.com>
2025-01-26 14:10:50 +0100

    Faqs

--- src/app/pages/technical-service/technical-service.component.ts
+++ src/app/pages/technical-service/technical-service.component.ts
@@ -3,14 +3,15 @@ import { TopComponent } from '../../shaders/top/top.component';
 import { AboutComponent } from './about/about.component';
 import { ServicesComponent } from './services/services.component';
 import { GoContactComponent } from '../../shaders/go-contact/go-contact.component';
+import { FaqsComponent } from "../../shaders/faqs/faqs.component";
 
 @Component({
   selector: 'app-technical-service',
   standalone: true,
-  imports: [TopComponent, AboutComponent, ServicesComponent, GoContactComponent],
+  imports: [TopComponent, AboutComponent, ServicesComponent, GoContactComponent, FaqsComponent],
   templateUrl: './technical-service.component.html',
   styleUrl: './technical-service.component.scss'
 })
 export class TechnicalServiceComponent {
-
+  
 }
