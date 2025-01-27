1c5432334d6fd90061640621b2f479848dcd207e
Egor <egsrapps@gmail.com>
2025-01-28 00:07:17 +0100

    Menu

--- src/app/shaders/header/header.component.ts
+++ src/app/shaders/header/header.component.ts
@@ -1,13 +1,40 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
-import { RouterModule } from '@angular/router';
+import { NavigationEnd, Router, RouterModule } from '@angular/router';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-header',
   standalone: true,
-  imports: [RouterModule],
+  imports: [CommonModule, RouterModule],
   templateUrl: './header.component.html',
   styleUrl: './header.component.scss'
 })
 export class HeaderComponent {
+
+  opened : boolean = false;
+
+  private routerSubscription!: Subscription;
+
+  constructor(private _router: Router) {}
+
+  ngOnInit(): void {
+    this.routerSubscription = this._router.events.subscribe(event => {
+      if (event instanceof NavigationEnd) {
+        this.opened = false;
+      }
+    });
+  }
+
+  ngOnDestroy(): void {
+    if (this.routerSubscription) {
+      this.routerSubscription.unsubscribe();
+    }
+  }
+
+  setOpened(state : boolean) {
+    this.opened = state;
+    console.log('change');
+  }
 
 }
