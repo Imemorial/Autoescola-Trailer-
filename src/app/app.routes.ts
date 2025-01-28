000c6891b2790a55c1eabb725a6d289060cbf469
Egor <egsrapps@gmail.com>
2025-01-28 22:38:41 +0100

    Whatsapp

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,11 +2,11 @@ import { Routes } from '@angular/router';
 
 export const routes: Routes = [
     {
-        path: 'home',
+        path: 'inicio',
         loadComponent: () => import('./pages/home/home.component').then((c) => c.HomeComponent)
     },
     {
-        path: 'websiteDesign',
+        path: 'diseno-web',
         loadComponent: () => import('./pages/website-design/website-design.component').then((c) => c.WebsiteDesignComponent)
     },
     {
@@ -14,19 +14,19 @@ export const routes: Routes = [
         loadComponent: () => import('./pages/software/software.component').then((c) => c.SoftwareComponent)
     },
     {
-        path: 'ecommerce',
+        path: 'comercio-electronico',
         loadComponent: () => import('./pages/ecommerce/ecommerce.component').then((c) => c.EcommerceComponent)
     },
     {
-        path: 'social',
+        path: 'redes-sociales',
         loadComponent: () => import('./pages/social-media/social-media.component').then((c) => c.SocialMediaComponent)
     },
     {
-        path: 'cameras',
+        path: 'camaras',
         loadComponent: () => import('./pages/cameras/cameras.component').then((c) => c.CamerasComponent)
     },
     {
-        path: 'technicalService',
+        path: 'servicio-tecnico',
         loadComponent: () => import('./pages/technical-service/technical-service.component').then((c) => c.TechnicalServiceComponent)
     },
     {
@@ -34,24 +34,28 @@ export const routes: Routes = [
         loadComponent: () => import('./pages/ia/ia.component').then((c) => c.IaComponent)
     },
     {
-        path: 'contact',
+        path: 'contacto',
         loadComponent: () => import('./pages/contact/contact.component').then((c) => c.ContactComponent)
     },
     {
-        path: 'about-us',
+        path: 'nosotros',
         loadComponent: () => import('./pages/about-us/about-us.component').then((c) => c.AboutUsComponent)
     },
     {
-        path: 'terms',
+        path: 'terminos',
         loadComponent: () => import('./pages/terms/terms.component').then((c) => c.TermsComponent)
     },
     {
-        path: 'policy',
+        path: 'politica',
         loadComponent: () => import('./pages/policity/policity.component').then((c) => c.PolicityComponent)
     },
     {
+        path: 'cookies',
+        loadComponent: () => import('./pages/cookies/cookies.component').then((c) => c.CookiesComponent)
+    },
+    {
         path: '',
-        redirectTo: 'home',
+        redirectTo: 'inicio',
         pathMatch: 'full'
     }
 ];
