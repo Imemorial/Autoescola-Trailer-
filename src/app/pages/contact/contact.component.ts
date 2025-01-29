e57be9963237fb59fb9d73506840f4cd5377db42
Egor <egsrapps@gmail.com>
2025-01-29 16:49:22 +0100

    Form

--- src/app/pages/contact/contact.component.ts
+++ src/app/pages/contact/contact.component.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
+import { TopComponent } from '../../shaders/top/top.component';
+import { ContactComponent as Contact } from '../../shaders/contact/contact.component';
 
 @Component({
-  selector: 'app-contact',
+  selector: 'app-contact-page',
   standalone: true,
-  imports: [],
+  imports: [TopComponent, Contact],
   templateUrl: './contact.component.html',
   styleUrl: './contact.component.scss'
 })
