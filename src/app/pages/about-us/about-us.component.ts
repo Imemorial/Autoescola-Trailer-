4c7931ce6a234c81dc5b305b67f197942b0973dd
Egor <egsrapps@gmail.com>
2025-01-29 16:14:49 +0100

    New About

--- src/app/pages/about-us/about-us.component.ts
+++ src/app/pages/about-us/about-us.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { AboutUsInfoComponent } from './about-us-info/about-us-info.component';
 import { TopComponent } from '../../shaders/top/top.component';
+import { ContactComponent } from '../../shaders/contact/contact.component';
 
 @Component({
   selector: 'app-about-us',
   standalone: true,
-  imports: [TopComponent, AboutUsInfoComponent],
+  imports: [TopComponent, AboutUsInfoComponent, ContactComponent],
   templateUrl: './about-us.component.html',
   styleUrl: './about-us.component.scss'
 })
