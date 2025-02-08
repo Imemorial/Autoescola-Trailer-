71b9edff1bcd9c0be7544b12c2fc52977ba00c91
egorsr <=>
2025-02-08 05:18:54 +0100

    Faqs

--- src/app/pages/home/components/faqs/faqs.component.ts
+++ src/app/pages/home/components/faqs/faqs.component.ts
@@ -1,40 +1,26 @@
-import { NgFor, NgIf } from '@angular/common';
+import { NgClass, NgFor, NgIf } from '@angular/common';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-faqs',
   standalone: true,
-  imports: [NgFor, NgIf],
+  imports: [NgFor, NgIf, NgClass],
   templateUrl: './faqs.component.html',
   styleUrl: './faqs.component.scss'
 })
 export class FaqsComponent {
 
-  faqItems = [
-    {
-      question: 'Quins permisos oferiu per a tràilers?',
-      answer: 'Oferim cursos per obtenir el permís de tràiler classe C1 i C.',
-      open: false
-    },
-    {
-      question: 'Quines classes de vehicles es poden conduir amb el permís de tràiler?',
-      answer: 'Amb el permís de tràiler, pots conduir vehicles que tinguin una massa màxima autoritzada de fins a 3.500 kg i 4.250 kg amb remolc.',
-      open: false
-    },
-    {
-      question: 'Es pot fer el curs en línia?',
-      answer: 'Sí, oferim cursos teòrics en línia per facilitar la formació des de casa.',
-      open: false
-    },
-    {
-      question: 'On es troba la vostra autoescola?',
-      answer: 'La nostra autoescola es troba a Mollerussa, a la província de Lleida.',
-      open: false
-    }
+  faqs = [
+    { question: 'Quins permisos de conduir puc obtenir a Autoescola Tràiler?', answer: 'A la nostra autoescola pots obtenir permisos de conducció com el B, C, C+E, D i permisos especials.', open: false },
+    { question: "Com puc inscriure'm als cursos de conducció?", answer: "Pots inscriure’t presencialment a la nostra oficina de Mollerussa o a través de la nostra pàgina web.", open: false },
+    { question: 'Quins són els horaris de les classes?', answer: 'Les classes teòriques són de dilluns a divendres en diferents horaris, i les pràctiques s’adapten a la disponibilitat.', open: false },
+    { question: "Què necessito per presentar-me a l'examen?", answer: "Has de completar les classes teòriques, superar els tests pràctics i tenir la documentació en regla.", open: false }
   ];
 
-  toggleAnswer(item: any): void {
-    item.open = !item.open;
+  toggleFaq(index: number) {
+    console.log(index)
+
+    this.faqs[index].open = !this.faqs[index].open;
   }
 
 }
