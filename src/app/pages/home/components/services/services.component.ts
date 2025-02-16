d657a9550b115f6f3ececce817008154933b0e1b
Egor <egsrapps@gmail.com>
2025-02-16 17:12:44 +0100

    Permisos

--- src/app/pages/home/components/services/services.component.ts
+++ src/app/pages/home/components/services/services.component.ts
@@ -44,33 +44,72 @@ export class ServicesComponent {
       image: '/assets/icons/ce.png',
       title: 'Carnet C+E',
     },
-  
-];
-  
+  ];
+
+  others: any[] = [
+    {
+      image: '/assets/icons/cap.png',
+      title: 'Permís CAP',
+    },
+    {
+      image: '/assets/icons/adr.png',
+      title: 'Permís ADR',
+    },
+    {
+      image: '/assets/icons/tractor.png',
+      title: 'LVA',
+    },
+  ];
+
   constructor() {}
+
   ngAfterViewInit(): void {
-    const splide = new Splide('.splide', {
+    const splide1 = new Splide('.splide', {
       type: 'loop',
-      perPage: 4,         
+      perPage: 4,
       perMove: 1,
       autoplay: true,
       arrows: false,
       gap: '1em',
       pagination: false,
+      interval: 2000,
       breakpoints: {
-        768: {           
-          perPage: 1,      
+        768: {
+          perPage: 1,
         },
       },
     }).mount();
-  
-    const prevButton = document.getElementById('slider-button-left');
-    const nextButton = document.getElementById('slider-button-right');
-  
-    if (prevButton && nextButton) {
-      prevButton.addEventListener('click', () => splide.go('<'));
-      nextButton.addEventListener('click', () => splide.go('>'));
+
+    const prevButton1 = document.getElementById('slider-button-left');
+    const nextButton1 = document.getElementById('slider-button-right');
+
+    if (prevButton1 && nextButton1) {
+      prevButton1.addEventListener('click', () => splide1.go('<'));
+      nextButton1.addEventListener('click', () => splide1.go('>'));
+    }
+
+    const splide2 = new Splide('.splide2', {
+      type: 'loop',
+      perPage: 2,
+      perMove: 1,
+      autoplay: true,
+      arrows: false,
+      gap: '1em',
+      pagination: false,
+      interval: 3000,
+      breakpoints: {
+        768: {
+          perPage: 1,
+        },
+      },
+    }).mount();
+
+    const prevButton2 = document.getElementById('slider-button-left2');
+    const nextButton2 = document.getElementById('slider-button-right2');
+
+    if (prevButton2 && nextButton2) {
+      prevButton2.addEventListener('click', () => splide2.go('<'));
+      nextButton2.addEventListener('click', () => splide2.go('>'));
     }
   }
-
 }
