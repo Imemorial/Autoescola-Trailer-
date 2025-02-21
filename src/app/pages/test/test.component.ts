f06ed227bf4ce8ee1a54b6429d5294119d91496b
Egor <egsrapps@gmail.com>
2025-02-21 16:34:16 +0100

    Select

--- src/app/pages/test/test.component.ts
+++ src/app/pages/test/test.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { SelectComponent } from './select/select.component';
 
 @Component({
   selector: 'app-test',
   standalone: true,
-  imports: [],
+  imports: [SelectComponent],
   templateUrl: './test.component.html',
   styleUrl: './test.component.scss'
 })
