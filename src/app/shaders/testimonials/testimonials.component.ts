1eb618338f8919eac15d83ce1ab71469b72f8150
Egor <egsrapps@gmail.com>
2025-02-22 13:57:14 +0100

    Up

--- src/app/shaders/testimonials/testimonials.component.ts
+++ src/app/shaders/testimonials/testimonials.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, ViewChild } from '@angular/core';
 import Splide from '@splidejs/splide';
 
 @Component({
@@ -10,19 +10,31 @@ import Splide from '@splidejs/splide';
 })
 export class TestimonialsComponent {
 
+  @ViewChild('splide', { static: true }) splideElement!: ElementRef<HTMLDivElement>;
+
+  private splideInstance!: Splide;
+
   constructor() { }
 
   ngOnInit(): void {
-    new Splide('#testimonial-slider', {
+    this.splideInstance = new Splide('#testimonial-slider', {
       type       : 'loop',
       perPage    : 3,
       focus      : 'center',
       pagination: false,
-      arrows     : true,
+      arrows     : false,
       prevArrow  : '#slider-button-left',
       nextArrow  : '#slider-button-right',
       gap: '1em'
     }).mount();
+
+    const prevButton = document.getElementById('slider-button-left-testimonial');
+    const nextButton = document.getElementById('slider-button-right-testimonial');
+
+    if (prevButton && nextButton) {
+      prevButton.addEventListener('click', () => this.splideInstance.go('<'));
+      nextButton.addEventListener('click', () => this.splideInstance.go('>'));
+    }
   }
 
 }
