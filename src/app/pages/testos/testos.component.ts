115ec628a6ebb0fd2a9401e301a3425706110bf1
Egor <egsrapps@gmail.com>
2025-02-23 16:28:15 +0100

    Animation

--- src/app/pages/testos/testos.component.ts
+++ src/app/pages/testos/testos.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { TopComponent } from '../../shaders/top/top.component';
 import { TestComponent } from './components/test/test.component';
+import { ModalComponent } from './components/modal/modal.component';
 
 @Component({
   selector: 'app-testos',
   standalone: true,
-  imports: [TopComponent, TestComponent],
+  imports: [TopComponent, TestComponent, ModalComponent],
   templateUrl: './testos.component.html',
   styleUrl: './testos.component.scss'
 })
