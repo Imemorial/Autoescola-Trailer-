b9472a73a583350e094bc0bd596107cbcc06f0c2
Egor <egsrapps@gmail.com>
2025-02-23 16:31:24 +0100

    No Modal

--- src/app/pages/testos/testos.component.ts
+++ src/app/pages/testos/testos.component.ts
@@ -1,12 +1,11 @@
 import { Component } from '@angular/core';
 import { TopComponent } from '../../shaders/top/top.component';
 import { TestComponent } from './components/test/test.component';
-import { ModalComponent } from './components/modal/modal.component';
 
 @Component({
   selector: 'app-testos',
   standalone: true,
-  imports: [TopComponent, TestComponent, ModalComponent],
+  imports: [TopComponent, TestComponent],
   templateUrl: './testos.component.html',
   styleUrl: './testos.component.scss'
 })
