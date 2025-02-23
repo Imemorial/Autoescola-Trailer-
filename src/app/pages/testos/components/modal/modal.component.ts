ea27a1b490fd5d2a7ecfa0c0756e86a01e38e6eb
Egor <egsrapps@gmail.com>
2025-02-23 16:30:12 +0100

    Close Effect

--- src/app/pages/testos/components/modal/modal.component.ts
+++ src/app/pages/testos/components/modal/modal.component.ts
@@ -1,12 +1,21 @@
+import { NgIf } from '@angular/common';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-modal',
   standalone: true,
-  imports: [],
+  imports: [NgIf],
   templateUrl: './modal.component.html',
   styleUrl: './modal.component.scss'
 })
 export class ModalComponent {
+
+  opened : boolean = false;
+
+  ngOnInit(){
+    setTimeout(() => {
+      this.opened =! this.opened;
+    }, 1500);
+  }
 
 }
