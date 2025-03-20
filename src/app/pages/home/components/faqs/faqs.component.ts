a801162a9241bd337271797e7d16f9d93117dd7a
Egor <egsrapps@gmail.com>
2025-03-20 16:41:07 +0100

    Faqs

--- src/app/pages/home/components/faqs/faqs.component.ts
+++ src/app/pages/home/components/faqs/faqs.component.ts
@@ -11,7 +11,7 @@ import { Component } from '@angular/core';
 export class FaqsComponent {
 
   faqs = [
-    { question: 'Quins permisos de conduir puc obtenir a Autoescola Tràiler?', answer: 'A la nostra autoescola pots obtenir permisos de conducció com el B, C, C+E, D i permisos especials.', open: false },
+    { question: "Quins documents necessito per inscriure'm?", answer: 'DNI, NIE o passaport, certificat mèdic, fotos de carnet.', open: false },
     { question: "Com puc inscriure'm als cursos de conducció?", answer: "Pots inscriure’t presencialment a la nostra oficina de Mollerussa o a través de la nostra pàgina web.", open: false },
     { question: 'Quins són els horaris de les classes?', answer: 'Les classes teòriques són de dilluns a divendres en diferents horaris, i les pràctiques s’adapten a la disponibilitat.', open: false },
     { question: "Què necessito per presentar-me a l'examen?", answer: "Has de completar les classes teòriques, superar els tests pràctics i tenir la documentació en regla.", open: false }
