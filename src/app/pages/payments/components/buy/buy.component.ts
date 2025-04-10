af25c6c6041d40eed59936622eb5b556e930a530
Egor <egsrapps@gmail.com>
2025-04-10 17:43:01 +0200

    Payments

--- src/app/pages/payments/components/buy/buy.component.ts
+++ src/app/pages/payments/components/buy/buy.component.ts
@@ -1,12 +1,23 @@
 import { Component } from '@angular/core';
+import { ServicesService } from '../../../../services/services/services.service';
+import { Service } from '../../../../models/service';
+import { NgFor } from '@angular/common';
 
 @Component({
   selector: 'app-buy',
   standalone: true,
-  imports: [],
+  imports: [NgFor],
   templateUrl: './buy.component.html',
   styleUrl: './buy.component.scss'
 })
 export class BuyComponent {
+
+  services !: Service [];
+
+  constructor (private _service : ServicesService) {}
+
+  ngOnInit () {
+    this.services = this._service.getAllServices();
+  }
 
 }
