617fccf441b8a6671d7f0c2734717dee04765273
Egor <egsrapps@gmail.com>
2025-04-12 13:16:56 +0200

    Test

--- src/app/pages/about-us/about-us-info/about-us-info.component.ts
+++ src/app/pages/about-us/about-us-info/about-us-info.component.ts
@@ -1,12 +1,24 @@
-import { Component } from '@angular/core';
+import { NgIf } from '@angular/common';
+import { Component, EventEmitter, Output } from '@angular/core';
 
 @Component({
   selector: 'app-about-us-info',
   standalone: true,
-  imports: [],
+  imports: [NgIf],
   templateUrl: './about-us-info.component.html',
   styleUrl: './about-us-info.component.scss'
 })
 export class AboutUsInfoComponent {
+
+  @Output() stateModal: EventEmitter<boolean> = new EventEmitter<boolean>();
+    dropdownOpen: boolean = false;
+    
+    open() {
+      this.stateModal.emit(true);
+    }
+  
+    toggleDropdown() {
+      this.dropdownOpen = !this.dropdownOpen;
+    }
 
 }
