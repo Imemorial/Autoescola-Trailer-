9860fed6b1c47f3618ddd76a8e58aa17cb17c55b
MarcIT48 <e4381417@gmail.com>
2025-04-17 18:03:30 +0200

    Mensaje de commit

--- src/app/pages/policity/policity.component.ts
+++ src/app/pages/policity/policity.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-policity',
   standalone: true,
-  imports: [],
+  imports: [RouterModule],
   templateUrl: './policity.component.html',
   styleUrl: './policity.component.scss'
 })
