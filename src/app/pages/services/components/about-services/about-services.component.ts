96920eedad99b4d11d1c19aff47104f35c64799e
Egor <egsrapps@gmail.com>
2025-04-25 00:00:07 +0200

    Content

--- src/app/pages/services/components/about-services/about-services.component.ts
+++ src/app/pages/services/components/about-services/about-services.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-about-services',
   standalone: true,
-  imports: [],
+  imports: [RouterModule],
   templateUrl: './about-services.component.html',
   styleUrl: './about-services.component.scss'
 })
