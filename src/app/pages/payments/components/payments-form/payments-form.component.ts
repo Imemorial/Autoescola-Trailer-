936f8d6da75c5526b87e85760a21ed3348f3f424
Egor <egsrapps@gmail.com>
2025-05-20 16:32:36 +0200

    Animate On Scroll

--- src/app/pages/payments/components/payments-form/payments-form.component.ts
+++ src/app/pages/payments/components/payments-form/payments-form.component.ts
@@ -1,19 +1,23 @@
 import { Component } from '@angular/core';
 import { FormBuilder, FormGroup, Validators, ReactiveFormsModule } from '@angular/forms';
 import { CommonModule } from '@angular/common';
+import { PaymentsService } from '../../../../services/payments/payments.service';
 
 @Component({
   selector: 'app-payments-form',
   standalone: true,
-  imports: [CommonModule, ReactiveFormsModule], // ✅ Aquí és clau!
+  imports: [CommonModule, ReactiveFormsModule],
   templateUrl: './payments-form.component.html',
   styleUrl: './payments-form.component.scss'
 })
 export class PaymentsFormComponent {
   form: FormGroup;
 
-  constructor(private fb: FormBuilder) {
-    this.form = this.fb.group({
+  constructor(
+    private _formBuilder: FormBuilder,
+    private _paymentsService: PaymentsService
+  ) {
+    this.form = this._formBuilder.group({
       studentName: ['', Validators.required],
       description: ['', Validators.required],
       amount: ['', [Validators.required, Validators.min(0.01)]]
@@ -22,7 +26,8 @@ export class PaymentsFormComponent {
 
   onSubmit(): void {
     if (this.form.valid) {
-      alert('Pagament registrat correctament!');
+      const amount = parseFloat(this.form.value.amount);
+      this._paymentsService.checkout(amount);
       this.form.reset();
     }
   }
