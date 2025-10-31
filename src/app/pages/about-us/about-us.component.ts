210ffd525c25d77843f0952bb71851efbc892f42
egor <egor@itnovastudio.com>
2025-10-31 16:38:44 +0100

    about us

--- src/app/pages/about-us/about-us.component.ts
+++ src/app/pages/about-us/about-us.component.ts
@@ -3,12 +3,11 @@ import { AboutUsInfoComponent } from './about-us-info/about-us-info.component';
 import { TopComponent } from '../../shaders/top/top.component';
 import { ContactComponent } from '../../shaders/contact/contact.component';
 import { Meta, Title } from '@angular/platform-browser';
-import { OurTeamComponent } from './our-team/our-team.component';
 
 @Component({
   selector: 'app-about-us',
   standalone: true,
-  imports: [TopComponent, AboutUsInfoComponent, ContactComponent, OurTeamComponent],
+  imports: [TopComponent, AboutUsInfoComponent, ContactComponent],
   templateUrl: './about-us.component.html',
   styleUrl: './about-us.component.scss'
 })
--- src/app/pages/services/services.component.ts
+++ src/app/pages/services/services.component.ts
@@ -1,7 +1,6 @@
 import { Component } from '@angular/core';
 import { TopComponent } from '../../shaders/top/top.component';
 import { ServicesComponent as ServicesComponentsShaders } from '../../shaders/services/services.component';
-import { NgFor } from '@angular/common';
 import { AboutServicesComponent } from "./components/about-services/about-services.component";
 import { StatsComponent } from './components/stats/stats.component';
 import { LostComponent } from './components/lost/lost.component';
@@ -10,7 +9,7 @@ import { SeoService } from '../../services/seo/seo.service';
 @Component({
   selector: 'app-services',
   standalone: true,
-  imports: [TopComponent, NgFor, AboutServicesComponent, ServicesComponentsShaders, StatsComponent, LostComponent],
+  imports: [TopComponent, AboutServicesComponent, ServicesComponentsShaders, StatsComponent, LostComponent],
   templateUrl: './services.component.html',
   styleUrl: './services.component.scss'
 })
